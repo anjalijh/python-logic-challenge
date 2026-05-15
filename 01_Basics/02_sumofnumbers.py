01a59ff41d8b9dd4017eba7f25a78a7d14cd3153
AnjaliJadhav <sycs24025anjali@gmail.com>
2026-05-15 18:52:44 +0530

    02_sumofnumbers.py

---
+++ 01_Basics/02_sumofnumbers.py
@@ -0,0 +1,8 @@
+# Challenge 02 : Sum of two numbers
+# Ask the user to input two numbers 
+n1=int(input("Enter the first number:"))
+n2=int(input("Enter the second number:"))
+# Use sum to do the addition and store the answer in sum variable
+sum=n1+n2
+# Display the output to the user
+print(n1,"+",n2,"=",sum)
